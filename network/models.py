7dbb916246fe0cf1954f38e994c5102a6bfbc9ae
Booklynn <95514331+Booklynn@users.noreply.github.com>
2024-12-21 19:56:49 +0700

    Added post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+# class Like(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+#     created_at = models.DateTimeField(auto_now_add=True)
